5923268cc6aff48c213779778c001e56b6d6c3fc
Juan Acosta <acosta.juan@icloud.com>
2026-09-24 10:19:11 -0400

    refactor: started refactoring the project so it can be access from root folder.

---
+++ common/validators.py
@@ -0,0 +1,38 @@
+# This module contains all the custom validators used throughout different tasks in 100 days of Python
+
+def validate_input(prompt: str, choices: list) -> str:
+    """
+        Validate that the user only inputs one of the posible choices.
+        Receives prompt message to display, and a set of possible choices.
+        Returns a valid choice.
+    """
+    valid_choices = choices
+    choice = input(prompt)
+    while choice not in valid_choices:
+        choice = input(f"Wrong input: Type {valid_choices}:\n - ")
+
+    return choice
+
+def validate_int(prompt: str) -> int:
+    """
+        Validates that the user only writes integers in their answer.
+        Receives prompt message to display.
+        Returns the integer.
+    """
+    while True:
+        user_input = input(prompt)
+        try:
+            return int(user_input)
+        except ValueError:
+            print("Wrong Input: Please enter a valid integer")
+
+def validate_float(prompt: str) -> int:
+    """
+        Validates that the user only writes integers in their answer.
+    """
+    while True:
+        user_input = input(prompt)
+        try:
+            return float(user_input)
+        except ValueError:
+            print("Wrong Input: Please enter a valid float")
